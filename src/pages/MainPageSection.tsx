f68c5a1d76634f3dd2f617acae7c321971c4d026
Prateek <prateekpm123@gmail.com>
2024-09-08 23:43:53 +0530

    Added the 2 sections for my page, which is the nodes section and normal page section

---
+++ src/pages/MainPageSection.tsx
@@ -0,0 +1,10 @@
+
+function MainPageSection() {
+    return (
+        <div className="h-1/2 w-full bg-inherit">
+            <h1 className="text-3xl text-slate-300 pt-3">Main page</h1>
+        </div>
+    )
+} 
+
+export default MainPageSection;
---
+++ src/pages/NodeSection.tsx
@@ -0,0 +1,10 @@
+
+function NodeSection() {
+    return(
+        <div className="h-1/3 w-full bg-slate-300">
+            <h2 className="text-3xl text-slate-800 pt-3">Node section</h2>
+        </div>
+    )
+}
+
+export default NodeSection;
